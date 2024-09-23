5aaa567ef5fb73418a2acff8cb7c917af90e418f
Jayanta Sarkar <jsarkar14683@gmail.com>
2024-09-23 10:19:36 +0530

    vue-carousel: updated build time config

--- vue-carousel-24/vite.config.ts
+++ vue-carousel-24/vite.config.ts
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    lib: {
+      entry: resolve(_dirname, "src/index.ts"),
+      name: "VueCarousel24",
+      fileName: "vue-carousel-24"
+    },
+    rollupOptions: {
+      external: ["vue"],
+      output: {
+        globals: {
+          vue: "Vue",
+        }
+      }
+    }
+  }
 })
